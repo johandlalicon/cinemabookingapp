0226d9e457b8c24d56b4a8c5a814fddf6b2a040d
johandlalicon <johandlalicon@hotmail.com>
2023-08-06 02:05:43 +0800

    add login auth feature and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/about'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
+  
 end
