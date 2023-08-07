3aa35c6b98a603fca83ac1a0e9e713b19ad901fb
johandlalicon <johandlalicon@hotmail.com>
2023-08-08 05:01:26 +0800

    add lil bit of stylings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    def button_style
+        "flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    end
+
+    def label_class
+        "block text-sm font-medium leading-6 text-gray-900"
+    end
+    def input_class
+        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    end
+    def form_button_class
+        "flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    end
+
+     # def display_validation_errors(model, attribute)
+    # if model.errors.any?
+    #   errors = model.errors.full_messages_for(attribute).join(', ')
+    #   content_tag(:div, errors, class: 'text-red-500')
+    # end
 end
--- app/models/screening.rb
+++ app/models/screening.rb
@@ -4,6 +4,10 @@ class Screening < ApplicationRecord
   belongs_to :timeslot
   has_many :bookings
 
+  validates :movie_id, presence: true
+  validates :cinema_id, presence: true
+  validates :timeslot_id, presence: true
+
   def available_seats
     booked_seats = bookings.sum(:seats)
     available = seating_capacity - booked_seats
