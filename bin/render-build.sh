0ee6f9061f25fbfa3b92632501ec8102faf98e36
johandlalicon <johandlalicon@hotmail.com>
2023-08-10 06:50:22 +0800

    ready deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,16 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate VERSION=0
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate VERSION=0
+# bundle exec rake db:migrate
+# bundle exec rake db:seed
