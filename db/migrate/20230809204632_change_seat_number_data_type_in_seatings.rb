ce2c039ccbea2cc00abe5f53f731775d57aca845
johandlalicon <johandlalicon@hotmail.com>
2023-08-10 07:04:58 +0800

    update db

--- db/migrate/20230809204632_change_seat_number_data_type_in_seatings.rb
+++ db/migrate/20230809204632_change_seat_number_data_type_in_seatings.rb
@@ -1,4 +1,9 @@
 class ChangeSeatNumberDataTypeInSeatings < ActiveRecord::Migration[7.0]
-  def change
-      change_column :seatings, :seat_number, :string
+  def up
+    change_column :seatings, :seat_number, :string
   end
+
+  def down
+    change_column :seatings, :seat_number, :integer
+  end
+end
