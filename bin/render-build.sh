bf06cfac8290d9f4524e121cc3dd3c25e7313937
johandlalicon <johandlalicon@hotmail.com>
2023-08-09 10:30:04 +0800

    update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:reset
+bundle exec rake db:migrate VERSION=0
 bundle exec rake db:migrate
 bundle exec rake db:seed
