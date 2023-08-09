a4b586a0224819bcdcaaaf2501d237fd9af43ac7
johandlalicon <johandlalicon@hotmail.com>
2023-08-09 10:37:09 +0800

    still fix error

--- db/migrate/20230805164516_create_screenings.rb
+++ db/migrate/20230805164516_create_screenings.rb
@@ -2,11 +2,10 @@ class CreateScreenings < ActiveRecord::Migration[7.0]
   def change
     create_table :screenings do |t|
       t.references :movie, null: false, foreign_key: true
-      t.references :cinema, null: false, foreign_key: true
+      t.references :cinema, null: false, foreign_key: { on_delete: :cascade }
       t.references :timeslot, null: false, foreign_key: true
       t.integer :seating_capacity
-      t.references :cinema, foreign_key: true, on_delete: :cascade
-      
+
       t.timestamps
     end
   end
