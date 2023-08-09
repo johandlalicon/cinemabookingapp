a92b25551b6debce97f186722316ff343f5171ed
johandlalicon <johandlalicon@hotmail.com>
2023-08-09 08:22:00 +0800

    update model

--- db/migrate/20230805164358_create_cinemas.rb
+++ db/migrate/20230805164358_create_cinemas.rb
@@ -3,7 +3,7 @@ class CreateCinemas < ActiveRecord::Migration[7.0]
     create_table :cinemas do |t|
       t.string :name
       t.string :address
-      t.references :mall, foreign_key: true, on_delete: :cascade
+      
       t.timestamps
     end
   end
